19bd911ccf3e648c369d47132d86275f3d69f5c5
164253 <66006890+164253@users.noreply.github.com>
2024-06-02 01:16:39 +0800

    remove get_batch and use batches*
    origin doesn't merge batch_size matrices
    copy matrix data to avoid modifying original data
    use batch_data array to simplify get_batch

--- dataloader.c
+++ dataloader.c
@@ -2,15 +2,11 @@
 #include <time.h>
 #include "dataloader.h"
 
-batch_data* get_batch(dataloader *loader, int num){
-    // return (batch_data){loader->};
-}
-
 dataloader* init_dataloader(Matrix **data, Matrix **label, int batch_size, int data_length, int shuffle){
     dataloader *loader = malloc(sizeof(dataloader));
     int batch_length = (data_length - 1) / batch_size + 1;
-    *loader = (dataloader){malloc(batch_length * sizeof(Matrix*)), malloc(batch_length * sizeof(Matrix*)), batch_size, data_length, malloc(data_length * sizeof(int)), get_batch};
-    for (int i = 0, *p = loader->indice; i < data_length; ++i)p[i] = i;
+    *loader = (dataloader){malloc(batch_length * sizeof(batch_data)), batch_size, data_length, malloc(data_length * sizeof(int)), get_batch};
+    for (int i = 0; i < data_length; ++i)loader->indice[i] = i;
     if (shuffle){
         srand(time(0));
         for (int i = 1, *p = loader->indice; i < n; ++i){
@@ -18,13 +14,24 @@ dataloader* init_dataloader(Matrix **data, Matrix **label, int batch_size, int d
             p[i]^=p[k]^=p[i]^=p[k];
         }
     }
-    for (int i = 0; i < batch_length; ++i){
-        loader->data[i] = malloc(sizeof(data * batch_size));
-        loader->label[i] = malloc(sizeof(data * batch_size));
-        for (int j = 0; j < batch_size; ++j){
-            loader->data[i][j] = data[p[i* batch_size + j]];
-            loader->label[i][j] = label[p[i * batch_size + j]];
+    int row = (*data)->row, col = (*data)->col * batch_size, full_batch_legth = data_length / batch_size, latest_matrix_num = data_length % batch_size;
+    for (int batch_num = 0; batch_num < batch_length; ++batch_num){
+        loader->batches[batch_num]->data = create(row, col);
+        loader->batches[batch_num]->label = create(row, col);
+        for (int matrix_num = 0; matrix_num < batch_size; ++matrix_num){
+            for(int i = 0; i < row; ++i)
+                for(int j = 0; j < col; ++j){
+                    loader->batches[batch_num]->data[i][j] = data[p[batch_num * batch_size + matrix_num]][i][j];
+                    loader->batches[batch_num]->label[i][j] = label[p[batch_num * batch_size + matrix_num]][i][j];
+                }
         }
+    }
+    for (int matrix_num = 0; matrix_num < latest_matrix_num; ++matrix_num){
+        for(int i = 0; i < row; ++i)
+            for(int j = 0; j < col; ++j){
+                loader->batches[full_batch_length]->data[i][j] = data[p[full_batch_length * batch_size + matrix_num]][i][j];
+                loader->batches[full_batch_length]->label[i][j] = label[p[full_batch_length * batch_size + matrix_num]][i][j];
+            }
     }
     return loader;
 }
