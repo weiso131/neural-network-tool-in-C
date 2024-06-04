3b772ea452aa9b1133baf3da5b111091111964d6
許育瑋 <blackfire106.1231@gmail.com>
2024-06-04 21:12:20 +0800

    Modify optimizer.c

--- optimizer.c
+++ optimizer.c
@@ -3,12 +3,20 @@
 
 Matrix* optimize(optim* self, Matrix* dw, double lr, Matrix* Vdw, Matrix* Sdw){
     Matrix *ans = copy(dw);
-    for (int i = 0, row = dw->row, col = dw->col; i < row; ++i)
-        for (int j = 0; j < col; ++j)
-            ans[i][j] -= lr * (self->beta1 * Vdw[i][j] + (1 - self->beta1) * dw[i][j]) / (sqrt(self->beta2 * Sdw[i][j] + (1 - self->beta2) * dw[i][j] * dw[i][j]) + self->epsilon);
+    for (int i = 0, row = dw->row, col = dw->col; i < row; ++i){
+        for (int j = 0; j < col; ++j){
+            Vdw->entry[i * col + j] = self->beta1 * Vdw->entry[i * col + j] + (1 - self->beta1) * dw->entry[i * col + j];
+            Sdw->entry[i * col + j] = self->beta2 * Sdw->entry[i * col + j] + (1 - self->beta2) * dw->entry[i * col + j] * dw->entry[i * col + j];
+            ans->entry[i * col + j] -= lr * Vdw->entry[i * col + j] / (sqrt(Sdw->entry[i * col + j]) + self->epsilon);
+        }
+            
+    }
+        
     return ans;
 }
 
 optim* init_Adam(double lr, double beta1, double beta2, double episode){
-    return (optim){lr, beta1, beta2, episode, optimize};
+    optim* adam = malloc(sizeof(optim));
+    *adam = (optim){lr, beta1, beta2, episode, optimize};
+    return adam;
 }
