3762530a66536a1a718fda5f946b8f7e2e3a306c
許育瑋 <blackfire106.1231@gmail.com>
2024-06-15 01:47:23 +0800

    Fix dataloader label problem

--- dataloader.c
+++ dataloader.c
@@ -8,7 +8,7 @@ dataloader* init_dataloader(Matrix **data, Matrix **label, int batch_size, int d
         printf("(*data)->col should be 1");
         return NULL;
     }
-    int batch_length = (data_length - 1) / batch_size + 1, row = (*data)->row, col;
+    int batch_length = (data_length - 1) / batch_size + 1;
     dataloader *loader = malloc(sizeof(dataloader));
     
     *loader = (dataloader){(batch_data**)malloc(batch_length * sizeof(batch_data*)), 
@@ -25,20 +25,20 @@ dataloader* init_dataloader(Matrix **data, Matrix **label, int batch_size, int d
     for (int batch_num = 0; batch_num <= data_length / batch_size; ++batch_num){
         
         bool remainData = (data_length - batch_num * batch_size) / batch_size == 0;
-        col = (1 - remainData) * batch_size + (remainData) * (data_length % batch_size);
+        int col = (1 - remainData) * batch_size + (remainData) * (data_length % batch_size);
 
         if (col == 0)
             break;
         loader->batches[batch_num] = malloc(sizeof(batch_data));
-        loader->batches[batch_num]->data = create(row, col);
-        loader->batches[batch_num]->label = create(row, col);
+        loader->batches[batch_num]->data = create((*data)->row, col);
+        loader->batches[batch_num]->label = create((*label)->row, col);
         
         for (int matrix_num = 0; matrix_num < col; ++matrix_num){
-            for(int i = 0; i < row; ++i){
-                for(int j = 0; j < col; ++j){
+            for(int j = 0; j < col; ++j){
+                for(int i = 0; i < (*data)->row; ++i)
                     loader->batches[batch_num]->data->entry[i * col + j] = data[loader->indice[batch_num * batch_size + j]]->entry[i];
+                for(int i = 0; i < (*label)->row; ++i)
                     loader->batches[batch_num]->label->entry[i * col + j] = label[loader->indice[batch_num * batch_size + j]]->entry[i];
-                }
             }
                 
         }
