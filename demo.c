7008d2f3285d6f3d9f6606e7ca3965f993700247
164253 <66006890+164253@users.noreply.github.com>
2024-06-15 19:30:56 +0800

    Fix segement fault with direct read json file.
    Assume that all non-numeric characters in the json file are eliminated, demo.c can run will.

--- demo.c
+++ demo.c
@@ -1,6 +1,5 @@
-#include "src/matrix/matrix.h"
 #include <stdio.h>
-
+#include "src/matrix/matrix.h"
 
 #define ERROR(format, ...) {fprintf(stderr, format, __VA_ARGS__);return NULL;}
 
@@ -11,23 +10,32 @@ Matrix **create_vectors(int nums, int each_len){
     return vectors;
 }
 
-Matrix **read_vectors(char* file){
-    int n, m;
+void free_vectors(Matrix **vectors, int len){
+    for (int i = 0; i < len; ++i)
+        free_matrix(vectors[i]);
+    free(vectors);
+}
+
+Matrix **read_vectors(char* file, int *row, int *col){
     FILE *fp = fopen(file, "r");
     if (!fp)ERROR("%s doesn't exist.\n", file);
-    if (fscanf(fp, "%d%d", &n, &m) < 2)ERROR("%s can't read the size of matrix.\n", file);
-    printf("%d, %d", n, m);
-    Matrix **vectors = create_vectors(n, m);
-    for (int i = 0; i < n; ++i)
-        for (int j = 0; j < m; ++j)
-            if (!fscanf(fp, "%lf", vectors[i]->entry + j))ERROR("unknown error while reading matrix in %s\n", file);
+    if (fscanf(fp, "%d %d", row, col) < 2)ERROR("%s can't read the size of matrix.\n", file);
+    Matrix **vectors = create_vectors(*row, *col);
+    for (int i = 0; i < *row; ++i)
+        for (int j = 0; j < *col; ++j)
+            if (!fscanf(fp, "%lf", vectors[i]->entry + j))
+                ERROR("unknown error while reading matrix in %s\n", file);
     return vectors;
 }
 
 int main(){
-    Matrix **data = read_vectors("data.json");
-    Matrix **target = read_vectors("target.json");
-
-    print_matrix(data[0]);
+    int data_row, data_col, target_row, target_col;
+    Matrix **data = read_vectors("data.json", &data_row, &data_col);
+    if (data == NULL)return -1;
+    Matrix **target = read_vectors("target.json", &target_row, &target_col);
+    if (target == NULL){
+        free_vectors(data, data_col);
+        return -1;
+    }
     return 0;
 }
