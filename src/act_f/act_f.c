8c370ab867e7b16a273a0650ea30978b34fb7f53
164253 <66006890+164253@users.noreply.github.com>
2024-06-17 23:18:00 +0800

    implement sigmoid init and matrix forward/backward

--- src/act_f/act_f.c
+++ src/act_f/act_f.c
@@ -21,3 +21,25 @@ Matrix* relu_backward(ReLU *self, Matrix *dz, Matrix *x, optim *optimizer){
     return output;
 
 }
+
+nn_node* init_sigmoid(){
+    Sigmoid* sigmoid = malloc(sizeof(Sigmoid));
+    *sigmoid = (Sigmoid){sigmoid_forward, sigmoid_backward};
+
+    nn_node* sigmoid_node = malloc(sizeof(nn_node));
+    *sigmoid_node = (nn_node){NULL, NULL, sigmoid, ACTIVATE};
+
+    return sigmoid_node;
+}
+
+Matrix* sigmoid_forward(Sigmoid *self, Matrix *x){
+    return apply(x, sigmoid_forward_impl);
+}
+Matrix* sigmoid_backward(Sigmoid *self, Matrix *dz, Matrix *x, optim *optimizer){
+    
+    Matrix* act_PD = apply(x, sigmoid_backward_impl);//activate function partial derivative
+    Matrix* output = dot(dz, act_PD);
+    free_matrix(act_PD);
+    return output;
+
+}
