70c409f603c951473653b884ff6bb5744ad398a6
164253 <66006890+164253@users.noreply.github.com>
2024-06-17 23:15:17 +0800

    implement sigmoid_impl

--- src/act_f/act_f_impl.c
+++ src/act_f/act_f_impl.c
@@ -1,5 +1,6 @@
 #include "act_f_impl.h"
 #include <stdint.h>
+#include <math.h>
 
 #define double_int union{double d;int64_t i;}
 
@@ -16,4 +17,12 @@ double relu_backward_impl(double x){
     double_int out = {.d = 1.0};
     out.i &= ~(ori.i >> 63);
     return out.d;
+}
+
+double sigmoid_forward_impl(double x){
+    return 1 / (1 + exp(-x));
+}
+double sigmoid_backward_impl(double x){
+    double s = sigmoid_forward_impl(x);
+    return s * (1 - s);
 }
